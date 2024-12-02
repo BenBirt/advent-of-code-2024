78d8fe521b4d56ad36957f7ea537e2e2b2c10c70
Ben Birt <benbirt@google.com>
2024-12-02 17:12:44 +0000

    day 1 part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,6 +1,6 @@
 #![feature(str_split_whitespace_remainder)]
 
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let contents = fs::read_to_string(
@@ -8,8 +8,8 @@ fn main() {
     )
     .expect("Couldn't read input");
 
-    let mut group_1: Vec<i32> = Vec::new();
-    let mut group_2: Vec<i32> = Vec::new();
+    let mut group_1 = Vec::new();
+    let mut group_2 = Vec::new();
 
     let mut pairs = contents.split("\n");
     pairs.by_ref().for_each(|line| {
@@ -19,6 +19,11 @@ fn main() {
         assert_eq!(location_ids.remainder(), None);
     });
 
+    // part_one(group_1, group_2);
+    part_two(group_1, group_2);
+}
+
+fn part_one(mut group_1: Vec<i32>, mut group_2: Vec<i32>) {
     group_1.sort();
     group_2.sort();
 
@@ -28,5 +33,22 @@ fn main() {
         let distance = (location_id_1 - location_id_2).abs();
         total_distance += distance;
     }
-    println!("total_distance={}", total_distance);
+    println!("Part 1: total_distance={}", total_distance);
+}
+
+fn part_two(group_1: Vec<i32>, group_2: Vec<i32>) {
+    let mut group_2_counts: HashMap<i32, i32> = HashMap::new();
+    for location_id in group_2.iter() {
+        let count = group_2_counts.entry(*location_id).or_insert(0);
+        *count += 1;
+    }
+
+    let mut total_similarity = 0;
+    for location_id in group_1.iter() {
+        if group_2_counts.contains_key(location_id) {
+            let similarity = location_id * group_2_counts[location_id];
+            total_similarity += similarity;
+        }
+    }
+    println!("Part 2: total_similarity={}", total_similarity);
 }
