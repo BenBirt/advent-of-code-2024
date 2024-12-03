1072682ba2b674e5a1325fe0d9caa51586dd9ef7
Ben Birt <benbirt@google.com>
2024-12-03 13:12:10 +0000

    day 3 part 2

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -10,12 +10,33 @@ fn main() {
     let re = Regex::new(r"mul\([0-9]+,[0-9]+\)").unwrap();
     let sum: i32 = re
         .find_iter(&contents)
-        .map(|re_match| {
-            let split: Vec<&str> = re_match.as_str().split(",").collect();
-            let first_num = &split[0][4..].parse::<i32>().unwrap();
-            let second_num = &split[1][..&split[1].len() - 1].parse::<i32>().unwrap();
-            return first_num * second_num;
-        })
+        .map(|re_match| do_mul(re_match.as_str()))
         .sum();
     println!("Part one: sum={}", sum);
+
+    let re2 = Regex::new(r"mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)").unwrap();
+    let mut enabled = true;
+    let sum2: i32 = re2
+        .find_iter(&contents)
+        .map(|re2_match| {
+            match re2_match.as_str() {
+                "do()" => enabled = true,
+                "don't()" => enabled = false,
+                _ => {
+                    if enabled {
+                        return do_mul(re2_match.as_str());
+                    }
+                }
+            }
+            return 0;
+        })
+        .sum();
+    println!("Part two: sum={}", sum2);
+}
+
+fn do_mul(input: &str) -> i32 {
+    let split: Vec<&str> = input.split(",").collect();
+    let first_num = &split[0][4..].parse::<i32>().unwrap();
+    let second_num = &split[1][..&split[1].len() - 1].parse::<i32>().unwrap();
+    return first_num * second_num;
 }
