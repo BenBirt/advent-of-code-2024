b8ddf6bd74d5ed16ba69b626442258812d250890
Ben Birt <benbirt@google.com>
2024-12-14 22:26:27 +0000

    day 14 part 2

--- day-14/src/main.rs
+++ day-14/src/main.rs
@@ -1,4 +1,4 @@
-use std::{fs, sync::LazyLock};
+use std::{collections::HashMap, fs, sync::LazyLock};
 
 use regex::Regex;
 
@@ -47,35 +47,83 @@ fn main() {
     )
     .expect("Couldn't read input");
 
-    let mut robots: Vec<Robot> = contents.split("\n").map(|line| Robot::new(line)).collect();
+    let robots: Vec<Robot> = contents.split("\n").map(|line| Robot::new(line)).collect();
 
     let x_len = 101;
     let y_len = 103;
 
-    robots.iter_mut().for_each(|robot| {
-        for _ in 0..100 {
-            robot.step(x_len, y_len);
-        }
-    });
+    let mut part_one_robots = robots.clone();
+    for _ in 0..100 {
+        tick(&mut part_one_robots, x_len, y_len);
+    }
+    println!(
+        "Part one: total safety factor={}",
+        compute_safety_factor(&part_one_robots, x_len, y_len)
+    );
 
+    let mut part_two_robots = robots.clone();
+    let mut lowest_safety_factor = compute_safety_factor(&part_two_robots, x_len, y_len);
+    let mut lowest_safety_factor_seconds = 0;
+    for second in 1..10000 {
+        tick(&mut part_two_robots, x_len, y_len);
+        let safety_factor = compute_safety_factor(&part_two_robots, x_len, y_len);
+        if safety_factor < lowest_safety_factor {
+            lowest_safety_factor = safety_factor;
+            lowest_safety_factor_seconds = second;
+        }
+    }
+    let mut part_two_robots = robots.clone();
+    for _ in 0..lowest_safety_factor_seconds {
+        tick(&mut part_two_robots, x_len, y_len);
+    }
+    print_robots(&part_two_robots, x_len, y_len);
+    println!(
+        "Part two: lowest_safety_factor_seconds={}", lowest_safety_factor_seconds
+    );
+}
+
+fn tick(robots: &mut Vec<Robot>, x_len: u32, y_len: u32) {
+    for robot in robots.iter_mut() {
+        robot.step(x_len, y_len);
+    }
+}
+
+fn compute_safety_factor(robots: &Vec<Robot>, x_len: u32, y_len: u32) -> i32 {
     let mut quadrant_1 = 0;
     let mut quadrant_2 = 0;
     let mut quadrant_3 = 0;
     let mut quadrant_4 = 0;
     for robot in robots {
-        if robot.position.0 < x_len/2 {
-            if robot.position.1 < y_len/2 {
+        if robot.position.0 < x_len / 2 {
+            if robot.position.1 < y_len / 2 {
                 quadrant_1 += 1;
-            } else if robot.position.1 > y_len/2 {
+            } else if robot.position.1 > y_len / 2 {
                 quadrant_2 += 1;
             }
-        } else if robot.position.0 > x_len/2 {
-            if robot.position.1 < y_len/2 {
+        } else if robot.position.0 > x_len / 2 {
+            if robot.position.1 < y_len / 2 {
                 quadrant_3 += 1;
-            } else if robot.position.1 > y_len/2 {
+            } else if robot.position.1 > y_len / 2 {
                 quadrant_4 += 1;
             }
         }
     }
-    println!("Part one: total safety factor={}", quadrant_1 * quadrant_2 * quadrant_3 * quadrant_4);
+    return quadrant_1 * quadrant_2 * quadrant_3 * quadrant_4;
+}
+
+fn print_robots(robots: &Vec<Robot>, x_len: u32, y_len: u32) {
+    let mut counters = HashMap::new();
+    for robot in robots {
+        *counters.entry(robot.position).or_insert(0) += 1;
+    }
+    for y in 0..y_len {
+        for x in 0..x_len {
+            if counters.contains_key(&(x, y)) {
+                print!("{}", counters[&(x, y)]);
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
 }
